4e630ec3d301c6d924651e903136ce2cdf6cb496
JoTech <77234307+JoTechOfficial@users.noreply.github.com>
2024-07-31 20:56:15 -0500

    Add files via upload

--- WJHRepairTool/Form1.Designer.cs
+++ WJHRepairTool/Form1.Designer.cs
@@ -39,13 +39,22 @@
             DefragmentDrivesbtn = new Button();
             DeleteTempFilesbtn = new Button();
             FixPrinterSpoolerbtn = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            GpuLabel = new Label();
+            CpuLabel = new Label();
+            RamLabel = new Label();
+            WindowsLabel = new Label();
+            BiosLabel = new Label();
             SuspendLayout();
             // 
             // SoundControlPanelbtn
             // 
-            SoundControlPanelbtn.Location = new Point(12, 12);
+            SoundControlPanelbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            SoundControlPanelbtn.Location = new Point(12, 65);
             SoundControlPanelbtn.Name = "SoundControlPanelbtn";
-            SoundControlPanelbtn.Size = new Size(270, 40);
+            SoundControlPanelbtn.Size = new Size(296, 58);
             SoundControlPanelbtn.TabIndex = 0;
             SoundControlPanelbtn.Text = "Sound Control Panel";
             SoundControlPanelbtn.UseVisualStyleBackColor = true;
@@ -53,9 +62,10 @@
             // 
             // NetworkSettingsbtn
             // 
-            NetworkSettingsbtn.Location = new Point(12, 58);
+            NetworkSettingsbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            NetworkSettingsbtn.Location = new Point(12, 129);
             NetworkSettingsbtn.Name = "NetworkSettingsbtn";
-            NetworkSettingsbtn.Size = new Size(270, 40);
+            NetworkSettingsbtn.Size = new Size(296, 58);
             NetworkSettingsbtn.TabIndex = 1;
             NetworkSettingsbtn.Text = "Network Settings";
             NetworkSettingsbtn.UseVisualStyleBackColor = true;
@@ -63,9 +73,10 @@
             // 
             // BluetoothSettingsbtn
             // 
-            BluetoothSettingsbtn.Location = new Point(12, 104);
+            BluetoothSettingsbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            BluetoothSettingsbtn.Location = new Point(12, 193);
             BluetoothSettingsbtn.Name = "BluetoothSettingsbtn";
-            BluetoothSettingsbtn.Size = new Size(270, 40);
+            BluetoothSettingsbtn.Size = new Size(296, 58);
             BluetoothSettingsbtn.TabIndex = 2;
             BluetoothSettingsbtn.Text = "Bluetooth Settings";
             BluetoothSettingsbtn.UseVisualStyleBackColor = true;
@@ -73,9 +84,10 @@
             // 
             // StartupAppsbtn
             // 
-            StartupAppsbtn.Location = new Point(12, 150);
+            StartupAppsbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            StartupAppsbtn.Location = new Point(12, 257);
             StartupAppsbtn.Name = "StartupAppsbtn";
-            StartupAppsbtn.Size = new Size(270, 40);
+            StartupAppsbtn.Size = new Size(296, 58);
             StartupAppsbtn.TabIndex = 3;
             StartupAppsbtn.Text = "Startup Apps";
             StartupAppsbtn.UseVisualStyleBackColor = true;
@@ -83,9 +95,10 @@
             // 
             // InstalledAppsbtn
             // 
-            InstalledAppsbtn.Location = new Point(12, 196);
+            InstalledAppsbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            InstalledAppsbtn.Location = new Point(12, 321);
             InstalledAppsbtn.Name = "InstalledAppsbtn";
-            InstalledAppsbtn.Size = new Size(270, 40);
+            InstalledAppsbtn.Size = new Size(296, 58);
             InstalledAppsbtn.TabIndex = 4;
             InstalledAppsbtn.Text = "Installed Apps";
             InstalledAppsbtn.UseVisualStyleBackColor = true;
@@ -93,9 +106,10 @@
             // 
             // DiskManagementbtn
             // 
-            DiskManagementbtn.Location = new Point(12, 245);
+            DiskManagementbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            DiskManagementbtn.Location = new Point(12, 385);
             DiskManagementbtn.Name = "DiskManagementbtn";
-            DiskManagementbtn.Size = new Size(270, 40);
+            DiskManagementbtn.Size = new Size(296, 58);
             DiskManagementbtn.TabIndex = 5;
             DiskManagementbtn.Text = "Disk Management";
             DiskManagementbtn.UseVisualStyleBackColor = true;
@@ -103,9 +117,10 @@
             // 
             // UpdateWindowsbtn
             // 
-            UpdateWindowsbtn.Location = new Point(353, 12);
+            UpdateWindowsbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            UpdateWindowsbtn.Location = new Point(352, 65);
             UpdateWindowsbtn.Name = "UpdateWindowsbtn";
-            UpdateWindowsbtn.Size = new Size(270, 40);
+            UpdateWindowsbtn.Size = new Size(296, 58);
             UpdateWindowsbtn.TabIndex = 6;
             UpdateWindowsbtn.Text = "Update Windows";
             UpdateWindowsbtn.UseVisualStyleBackColor = true;
@@ -113,9 +128,10 @@
             // 
             // DiskCleanupbtn
             // 
-            DiskCleanupbtn.Location = new Point(353, 58);
+            DiskCleanupbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            DiskCleanupbtn.Location = new Point(352, 129);
             DiskCleanupbtn.Name = "DiskCleanupbtn";
-            DiskCleanupbtn.Size = new Size(270, 40);
+            DiskCleanupbtn.Size = new Size(296, 58);
             DiskCleanupbtn.TabIndex = 7;
             DiskCleanupbtn.Text = "Disk Cleanup";
             DiskCleanupbtn.UseVisualStyleBackColor = true;
@@ -123,9 +139,10 @@
             // 
             // DefragmentDrivesbtn
             // 
-            DefragmentDrivesbtn.Location = new Point(353, 104);
+            DefragmentDrivesbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            DefragmentDrivesbtn.Location = new Point(352, 193);
             DefragmentDrivesbtn.Name = "DefragmentDrivesbtn";
-            DefragmentDrivesbtn.Size = new Size(270, 40);
+            DefragmentDrivesbtn.Size = new Size(296, 58);
             DefragmentDrivesbtn.TabIndex = 8;
             DefragmentDrivesbtn.Text = "Defragment Drives";
             DefragmentDrivesbtn.UseVisualStyleBackColor = true;
@@ -133,9 +150,10 @@
             // 
             // DeleteTempFilesbtn
             // 
-            DeleteTempFilesbtn.Location = new Point(353, 150);
+            DeleteTempFilesbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            DeleteTempFilesbtn.Location = new Point(352, 257);
             DeleteTempFilesbtn.Name = "DeleteTempFilesbtn";
-            DeleteTempFilesbtn.Size = new Size(270, 40);
+            DeleteTempFilesbtn.Size = new Size(296, 58);
             DeleteTempFilesbtn.TabIndex = 9;
             DeleteTempFilesbtn.Text = "Delete Temp Files";
             DeleteTempFilesbtn.UseVisualStyleBackColor = true;
@@ -143,19 +161,103 @@
             // 
             // FixPrinterSpoolerbtn
             // 
-            FixPrinterSpoolerbtn.Location = new Point(353, 196);
+            FixPrinterSpoolerbtn.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            FixPrinterSpoolerbtn.Location = new Point(352, 321);
             FixPrinterSpoolerbtn.Name = "FixPrinterSpoolerbtn";
-            FixPrinterSpoolerbtn.Size = new Size(270, 40);
+            FixPrinterSpoolerbtn.Size = new Size(296, 58);
             FixPrinterSpoolerbtn.TabIndex = 10;
             FixPrinterSpoolerbtn.Text = "Fix Printer Spooler";
             FixPrinterSpoolerbtn.UseVisualStyleBackColor = true;
             FixPrinterSpoolerbtn.Click += FixPrinterSpoolerbtn_Click;
             // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(12, 449);
+            button1.Name = "button1";
+            button1.Size = new Size(296, 58);
+            button1.TabIndex = 11;
+            button1.Text = "Disk Management";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.1428576F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(201, 45);
+            label1.TabIndex = 12;
+            label1.Text = "Easy Access";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.1428576F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(396, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(252, 45);
+            label2.TabIndex = 13;
+            label2.Text = "Tools and Fixes";
+            // 
+            // GpuLabel
+            // 
+            GpuLabel.AutoSize = true;
+            GpuLabel.Location = new Point(12, 556);
+            GpuLabel.Name = "GpuLabel";
+            GpuLabel.Size = new Size(68, 30);
+            GpuLabel.TabIndex = 14;
+            GpuLabel.Text = "label3";
+            // 
+            // CpuLabel
+            // 
+            CpuLabel.AutoSize = true;
+            CpuLabel.Location = new Point(12, 526);
+            CpuLabel.Name = "CpuLabel";
+            CpuLabel.Size = new Size(68, 30);
+            CpuLabel.TabIndex = 15;
+            CpuLabel.Text = "label3";
+            // 
+            // RamLabel
+            // 
+            RamLabel.AutoSize = true;
+            RamLabel.Location = new Point(12, 586);
+            RamLabel.Name = "RamLabel";
+            RamLabel.Size = new Size(68, 30);
+            RamLabel.TabIndex = 16;
+            RamLabel.Text = "label3";
+            // 
+            // WindowsLabel
+            // 
+            WindowsLabel.AutoSize = true;
+            WindowsLabel.Location = new Point(12, 616);
+            WindowsLabel.Name = "WindowsLabel";
+            WindowsLabel.Size = new Size(68, 30);
+            WindowsLabel.TabIndex = 17;
+            WindowsLabel.Text = "label3";
+            // 
+            // BiosLabel
+            // 
+            BiosLabel.AutoSize = true;
+            BiosLabel.Location = new Point(12, 646);
+            BiosLabel.Name = "BiosLabel";
+            BiosLabel.Size = new Size(68, 30);
+            BiosLabel.TabIndex = 18;
+            BiosLabel.Text = "label3";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(635, 298);
+            ClientSize = new Size(1376, 698);
+            Controls.Add(BiosLabel);
+            Controls.Add(WindowsLabel);
+            Controls.Add(RamLabel);
+            Controls.Add(CpuLabel);
+            Controls.Add(GpuLabel);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Controls.Add(FixPrinterSpoolerbtn);
             Controls.Add(DeleteTempFilesbtn);
             Controls.Add(DefragmentDrivesbtn);
@@ -170,6 +272,7 @@
             Name = "Form1";
             Text = "WJHRepairTool";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -185,5 +288,13 @@
         private Button DefragmentDrivesbtn;
         private Button DeleteTempFilesbtn;
         private Button FixPrinterSpoolerbtn;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label GpuLabel;
+        private Label CpuLabel;
+        private Label RamLabel;
+        private Label WindowsLabel;
+        private Label BiosLabel;
     }
 }
--- WJHRepairTool/Form1.cs
+++ WJHRepairTool/Form1.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Drawing;
+using System.Collections.Generic; // Add this for the Dictionary
 using System.Diagnostics;
-using System.Threading;
-using System.IO;
-using System.Text;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
-using Microsoft.Win32;
 
 namespace WJHRepairTool
 {
@@ -14,6 +12,25 @@ namespace WJHRepairTool
         public Form1()
         {
             InitializeComponent();
+
+            // Set GPU information when the form is loaded
+            GpuLabel.Text = "GPU: " + GetGPUName();
+
+            // Set CPU information when the form is loaded
+            string cpuInfo = ExecutePowerShellCommand("Get-WmiObject Win32_Processor | Select-Object -ExpandProperty Name");
+            CpuLabel.Text = "CPU: " + cpuInfo;
+
+            // Retrieve RAM information
+            string ramInfo = GetTotalPhysicalMemory();
+            RamLabel.Text = "Total System RAM: " + ramInfo;
+
+            // Retrieve Windows build information
+            string windowsVersion = GetWindowsBuildVersion();
+            WindowsLabel.Text = "OS Version: " + windowsVersion;
+
+            // Retrieve BIOS information using PowerShell
+            string biosInfo = GetBiosInformation();
+            BiosLabel.Text = "Mobo Name/BIOS Version: " + biosInfo;
         }
 
         private void SoundControlPanelbtn_Click(object sender, EventArgs e)
@@ -146,6 +163,177 @@ namespace WJHRepairTool
                 CreateNoWindow = false,  // Create a visible window
                 WindowStyle = ProcessWindowStyle.Normal  // Show the window in normal mode
             });
+        }
+
+        private string GetGPUName()
+        {
+            try
+            {
+                // Define the command to get GPU information
+                string command = "wmic path win32_videocontroller get caption";
+
+                // Create a process to execute the command
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/c " + command,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                // Start the process and read the output
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                // Parse the output to get the GPU name
+                string[] lines = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                if (lines.Length > 1)
+                {
+                    return lines[1].Trim(); // Return the GPU name, skipping the header
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur
+                MessageBox.Show("Error retrieving GPU information: " + ex.Message);
+            }
+
+            return "GPU information not available";
+        }
+
+        private string ExecutePowerShellCommand(string command)
+        {
+            try
+            {
+                // Create a process to execute the PowerShell command
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = $"-Command \"{command}\"",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                // Start the process and read the output
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                return output.Trim();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur
+                MessageBox.Show("Error retrieving information: " + ex.Message);
+            }
+
+            return "Information not available";
+        }
+
+        private string GetTotalPhysicalMemory()
+        {
+            try
+            {
+                // Get total physical memory (RAM) using Environment class
+                var ram = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+                return $"{ram / (1024 * 1024 * 1024)} GB"; // Convert bytes to GB
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur
+                MessageBox.Show("Error retrieving RAM information: " + ex.Message);
+            }
+
+            return "RAM information not available";
+        }
+
+        private string GetWindowsBuildVersion()
+        {
+            try
+            {
+                // Retrieve OS version and build number
+                Version osVersion = Environment.OSVersion.Version;
+                string versionString = osVersion.ToString();  // e.g., "10.0.19045"
+                string buildNumber = GetBuildNumber(); // Extract build number from OS version
+
+                // Map build numbers to Windows versions
+                string buildLabel = GetBuildLabel(buildNumber);
+
+                return $"{versionString} (Build {buildLabel})";
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur
+                MessageBox.Show("Error retrieving Windows build information: " + ex.Message);
+            }
+
+            return "Windows build information not available";
+        }
+
+        private string GetBuildNumber()
+        {
+            // Retrieve the build number from the OS version
+            // Adjust this as needed based on how the build number is retrieved
+            return Environment.OSVersion.Version.Build.ToString();
+        }
+
+        private string GetBuildLabel(string buildNumber)
+        {
+            // Define mappings for Windows versions
+            var buildLabels = new Dictionary<string, string>
+            {
+                // Windows 10
+    { "10240", "1507 (RTM)" },
+    { "10586", "1511" },
+    { "14393", "1607" },
+    { "15063", "1703" },
+    { "16299", "1709" },
+    { "17134", "1803" },
+    { "17763", "1809" },
+    { "18362", "1903" },
+    { "18363", "1909" },
+    { "19041", "2004" },
+    { "19042", "20H2" },
+    { "19043", "21H1" },
+    { "19044", "21H2" },
+    { "19045", "22H2" },
+
+    // Windows 11
+    { "22000", "21H2" },
+    { "22621", "22H2" },
+    { "22631", "23H2" }, // Adjusted from "24H2" based on current information
+            };
+
+            // Try to find the build number in the dictionary
+            if (buildLabels.TryGetValue(buildNumber, out string label))
+            {
+                return label;
+            }
+
+            return "Unknown Build";
+        }
+
+        private string GetBiosInformation()
+        {
+            string biosInfo = ExecutePowerShellCommand(@"
+        $bios = Get-WmiObject Win32_BIOS
+        $model = $bios.SMBIOSBIOSVersion
+
+        $baseboard = Get-WmiObject Win32_BaseBoard
+        $baseboardProduct = $baseboard.Product
+        $baseboardProduct + ' ' + $model
+    ");
+
+            return biosInfo.Trim();
         }
     }
 }
